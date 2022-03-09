9ae5a8ec32f8af6cca429a7fa5ee44c624f81c51
AlanDouglasDan <aland6209@gmail.com>
2022-03-09 20:31:11 +0100

    bug fixes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const routes = require("./routes/routes");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use("/translate", routes); 
 app.use(express.json());
 
 const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-})
+});
